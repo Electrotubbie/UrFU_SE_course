b8459b56729f010e31983034015e5ae979180288
user <steen2033@yandex.ru>
2023-11-28 01:39:08 +0500

    добавлен сервис API

---
+++ api.py
@@ -0,0 +1,17 @@
+from unmask import unmasker
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    text: str
+
+@app.get("/")
+async def root():
+    return {'message': 'С помощью рассматриваемой модели выполняется заполнение пропущенного слова на наиболее подходящее. \
+            В качестве входных данных принимается предложение с помеченным <mask> пропущенным словом.'}
+
+@app.post('/unmask/')
+async def unmask(item: Item):
+    return unmasker(item.text)
