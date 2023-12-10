4935caf37d06b889f55766236f08fc05df2f7df6
user <steen2033@yandex.ru>
2023-12-11 02:41:06 +0500

    try delpoy streamlit

---
+++ streamlit_app/streamlit_app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from transformers import pipeline
+
+unmasker = pipeline('fill-mask', model='xlm-roberta-base')
+
+st.title("Заполнение пропуска")
+st.divider()
+text = st.text_input("Введите предложение для заполнения пропуска согласно примеру", 
+	"Привет, я <mask> модель!")
+result = st.button("Заполнить!")
+if result:
+	variants = unmasker(text)
+	st.subheader("Результат подстановки:", divider='rainbow')
+	for variant in variants:
+		st.write(variant["sequence"])
