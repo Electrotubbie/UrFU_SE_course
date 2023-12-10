4a2dd41ea24cdf280a28bda591d4d58317680f62
user <steen2033@yandex.ru>
2023-12-11 01:44:36 +0500

    Del dependencies from streamlit

--- streamlit_web.py
+++ streamlit_web.py
@@ -1,5 +1,7 @@
 import streamlit as st
-from unmask import unmasker
+from transformers import pipeline
+
+unmasker = pipeline('fill-mask', model='xlm-roberta-base')
 
 st.title("Заполнение пропуска")
 st.divider()
